e35010245d14f91f032159fe59a413ac9491d83f
Jesse Chu <jesse.chu@outlook.com>
2025-08-19 17:13:50 -0400

    Add component Column to storybook.

---
+++ src/containers/Column.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Column from './Column';
+
+const meta: Meta<typeof Column> = {
+  title: 'Containers/Column',
+  component: Column,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: { action: 'clicked' },
+    className: {
+      control: 'text',
+    },
+    children: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <div className="p-4 bg-blue-100 rounded">Item 1</div>
+        <div className="p-4 bg-green-100 rounded">Item 2</div>
+        <div className="p-4 bg-yellow-100 rounded">Item 3</div>
+      </>
+    ),
+  },
+};
+
+export const WithCustomClassName: Story = {
+  args: {
+    children: (
+      <>
+        <div className="p-4 bg-red-100 rounded">Item 1</div>
+        <div className="p-4 bg-orange-100 rounded">Item 2</div>
+      </>
+    ),
+    className: 'p-6 border-2 border-gray-300 rounded-lg bg-gray-50',
+  },
+};
