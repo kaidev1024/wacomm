bed5b860e1c06a3fb4bfb2fc8b031e886a7fcdb5
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-11-09 10:44:43 -0800

    add LoadingIndicator

---
+++ src/loading/LoadingIndicator.tsx
@@ -0,0 +1,27 @@
+import { twcn } from 'waujs';
+import loadingGifUrl from './loading.gif';
+import Img from '../Img';
+import { Row } from 'src/containers';
+
+interface LoadingIndicatorProps {
+  className?: string;
+  imgClassName?: string;
+  labelClassName?: string;
+  label: string;
+}
+
+function LoadingIndicator({
+  className = '',
+  label,
+  imgClassName = '',
+  labelClassName = ''
+}: LoadingIndicatorProps) {
+  return (
+    <Row className={twcn('LoadingIndicator', className)}>
+      <Img src={loadingGifUrl} alt="loading" className={twcn('w-5', imgClassName)} />
+      <div className={twcn('ml-2', labelClassName)}>{label}</div>
+    </Row>
+  );
+}
+
+export default LoadingIndicator;
--- src/loading/index.ts
+++ src/loading/index.ts
@@ -1,1 +1,2 @@
 export { default as LoadingIcon } from './LoadingIcon';
+export { default as LoadingIndicator } from './LoadingIndicator';
