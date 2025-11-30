b350903df49dd1c19993366839151abc34f267b9
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-11-29 22:42:03 -0800

    fix letter size in LetterAvatar

--- src/LetterAvatar.tsx
+++ src/LetterAvatar.tsx
@@ -9,11 +9,33 @@ function LetterAvatar({ letter = 'A', className = 'w-full h-full' }: LetterAvata
   return (
     <div
       className={twcn(
-        'flex items-center justify-center rounded-full bg-blue-300 text-white font-bold',
+        'LetterAvatar flex items-center justify-center rounded-full bg-blue-300 text-white font-bold',
         className
       )}
     >
-      {letter.charAt(0).toUpperCase()}
+      {/* SVG scales the glyph to fill the container */}
+      <svg
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid meet"
+        width="100%"
+        height="100%"
+        role="img"
+        aria-label={letter.charAt(0).toUpperCase()}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text
+          x="50"
+          y="50"
+          textAnchor="middle"
+          dominantBaseline="central"
+          fontFamily="inherit"
+          fontWeight="700"
+          fontSize="80"
+          fill="currentColor"
+        >
+          {letter.charAt(0).toUpperCase()}
+        </text>
+      </svg>
     </div>
   );
 }
