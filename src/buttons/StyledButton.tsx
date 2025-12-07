5e147408fd48553d15fec413c7fb3897b8cf5216
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-12-06 17:09:53 -0800

    fix coloring of StyledButton

--- src/buttons/StyledButton.tsx
+++ src/buttons/StyledButton.tsx
@@ -1,10 +1,11 @@
-import { ZeroFuncType } from 'waujs';
+import { twcn, ZeroFuncType } from 'waujs';
 import Button from './Button';
 
 interface StyledButtonProps {
-  isHighlighted?: boolean | null;
+  isHighlighted?: boolean;
   isLoading?: boolean;
   disabled?: boolean;
+  className?: string;
   onClick: ZeroFuncType;
   label: string;
 }
@@ -23,17 +24,30 @@ interface StyledButtonProps {
 //   highlightedClassName: string;
 // }
 
+const getClassName = (isHighlighted: boolean, disabled: boolean) => {
+  if (isHighlighted) return 'bg-blue-500 border-0';
+  if (disabled) return 'bg-gray-300 border-gray-300 cursor-not-allowed';
+  return 'bg-white border-blue-500';
+};
+
+const getLabelClassName = (isHighlighted: boolean, disabled: boolean) => {
+  if (isHighlighted) return 'text-white text-sm';
+  if (disabled) return 'text-gray-500 text-sm';
+  return 'text-blue-500 text-sm';
+};
+
 function StyledButton({
   isHighlighted = false,
   isLoading = false,
   disabled = false,
+  className = '',
   onClick,
   label
 }: StyledButtonProps) {
   return (
     <Button
-      className={`w-fit h-fit ${isHighlighted ? 'bg-blue-500 border-0' : 'bg-white border-blue-500'}`}
-      labelClassName={`${isHighlighted ? 'text-white text-sm' : 'text-blue-500 text-sm'}`}
+      className={twcn('w-fit h-fit', getClassName(isHighlighted, disabled), className)}
+      labelClassName={twcn('text-sm', getLabelClassName(isHighlighted, disabled))}
       isLoading={isLoading}
       label={label}
       onClick={onClick}
