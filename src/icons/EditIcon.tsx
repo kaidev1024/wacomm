448cb5d54dcab52ab721a617dac067eb47ab30da
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-12-12 22:07:59 -0800

    minor fix

--- src/icons/EditIcon.tsx
+++ src/icons/EditIcon.tsx
@@ -5,15 +5,24 @@ interface EditIconProps {
   onClick?: ZeroFuncType;
   className?: string;
   hidden?: boolean;
+  isHighlighted?: boolean;
 }
 
-function EditIcon({ onClick = ZeroFunc, className = '', hidden = false }: EditIconProps) {
+function EditIcon({
+  onClick = ZeroFunc,
+  className = '',
+  hidden = false,
+  isHighlighted = false
+}: EditIconProps) {
   return (
     <div
       className={twcn(`w-5 hover:cursor-pointer ${hidden && 'hidden'}`, className)}
       onClick={onClick}
     >
-      <EditSvg size={SvgSize.sm} className={twcn('fill-gray-600', className)} />
+      <EditSvg
+        size={SvgSize.sm}
+        className={twcn(`${isHighlighted ? 'fill-blue-500' : 'fill-gray-600'}`, className)}
+      />
     </div>
   );
 }
