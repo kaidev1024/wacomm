13dd2dc84012ff0f85f253e274a83dd36b905512
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-12-14 17:58:55 -0800

    minor fix

--- src/expandableContainer/ExpandableContainerHeader.tsx
+++ src/expandableContainer/ExpandableContainerHeader.tsx
@@ -1,14 +1,15 @@
-import { AddSvg, SvgSize, CollapseSvg } from 'wasvg';
+import { AddSvg, SvgSize, MinusSvg } from 'wasvg';
 import { ZeroFuncType } from 'waujs';
 import { Row } from 'src/containers';
-import H1 from 'src/H1';
+import { ReactNode } from 'react';
 
 interface ExpandableContainerHeaderProps {
   isShown: boolean;
   onClick: ZeroFuncType;
-  title: string;
+  title: ReactNode;
   isExpandable?: boolean;
 }
+
 function ExpandableContainerHeader({
   isShown,
   onClick,
@@ -17,11 +18,11 @@ function ExpandableContainerHeader({
 }: ExpandableContainerHeaderProps) {
   return (
     <Row className="text-blue-500 justify-between py-0.5">
-      <H1 label={title} />
+      {title}
       {isExpandable && (
         <div onClick={onClick}>
           {isShown ? (
-            <CollapseSvg size={SvgSize.sm} className="fill-gray-888" />
+            <MinusSvg size={SvgSize.sm} className="fill-gray-888" />
           ) : (
             <AddSvg size={SvgSize.sm} className="fill-gray-888" />
           )}
