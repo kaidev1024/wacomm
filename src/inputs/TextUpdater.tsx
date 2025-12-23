7e10ed4a85f0a69882bd7b607f06aa8098d5f1c4
Xiaokai Li <xiaokai.li-1@hotmail.com>
2025-12-22 22:26:03 -0800

    minor fix

--- src/inputs/TextUpdater.tsx
+++ src/inputs/TextUpdater.tsx
@@ -1,28 +1,30 @@
 import { useState } from 'react';
-import { useEscKeydown } from 'waujs';
+import { twcn, useEscKeydown } from 'waujs';
 import TextareaAutosize from 'src/TextareaAutosize';
 import { Column, Row } from 'src/containers';
 import { EditIcon, P, SaveCancelButtons } from 'src';
 
 export interface TextUpdaterProps {
   value: string;
-  // eslint-disable-next-line no-unused-vars
   onSave: (v: string) => void;
   isLoading: boolean;
-  editable: boolean;
+  isEditing: boolean;
+  setIsEditing: (v: boolean) => void;
   placeholder?: string;
-  classNameEdit?: string;
-  classNameDisplay?: string;
+  classNameEditing?: string;
+  classNameNonEditing?: string;
 }
 
 function TextUpdater({
   value: valueInit,
   onSave,
   isLoading,
-  editable,
-  placeholder = 'A brief introduction...'
+  isEditing,
+  setIsEditing,
+  placeholder = 'A brief introduction...',
+  classNameEditing = '',
+  classNameNonEditing = ''
 }: TextUpdaterProps) {
-  const [isEditing, setIsEditing] = useState(false);
   const handleCancelEdit = () => setIsEditing(false);
   const [value, setValue] = useState(valueInit);
   useEscKeydown(() => {
@@ -30,8 +32,8 @@ function TextUpdater({
       handleCancelEdit();
     }
   });
-  return isEditing && editable ? (
-    <Column className="TextUpdaterEditing w-full gap-2">
+  return isEditing ? (
+    <Column className={twcn('TextUpdaterEditing w-full gap-2', classNameEditing)}>
       <TextareaAutosize
         minRows={3}
         maxRows={6}
@@ -52,7 +54,7 @@ function TextUpdater({
       />
     </Column>
   ) : (
-    <Row className="TextUpdaterNonEditing w-full gap-2 items-start">
+    <Row className={twcn('TextUpdaterNonEditing w-full gap-2 items-start', classNameNonEditing)}>
       <P value={value || placeholder} />
       <EditIcon className="cursor-pointer text-blue-500" onClick={() => setIsEditing(true)} />
     </Row>
