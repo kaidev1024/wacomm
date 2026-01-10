91073abade9a69db0dfbeac9d3adadee2880cd1c
Xiaokai Li <xiaokai.li-1@hotmail.com>
2026-01-10 11:25:00 -0800

    fix ModalItem

--- src/modal/ModalItem.tsx
+++ src/modal/ModalItem.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from 'react';
-import { Row } from 'src/containers';
+import { Column, Row } from 'src/containers';
 import { Span } from 'src/html';
 
 interface ModalItemProps {
   label: string;
+  optional?: ReactNode;
   children: ReactNode;
 }
 
-function ModalItem({ label, children }: ModalItemProps) {
+function ModalItem({ label, optional, children }: ModalItemProps) {
+  if (optional) {
+    return (
+      <Row className="w-full items-start">
+        <Column className="w-1/5">
+          <Span text={label} className="font-semibold" />
+          {optional && <div className="mt-2">{optional}</div>}
+        </Column>
+        <div className="w-4/5">{children}</div>
+      </Row>
+    );
+  }
   return (
     <Row className="w-full items-start">
       <Span text={label} className="text-gray-600 w-1/5" />
