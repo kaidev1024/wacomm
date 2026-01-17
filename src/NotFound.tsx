e000a34303752d8cfa1cfb3aa82849032cb5e984
Xiaokai Li <kaidev1024@gmail.com>
2026-01-16 21:17:22 -0800

    refactor

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,9 +1,18 @@
+import { twcn } from 'waujs';
+
 interface NotFoundProps {
   label: string;
+  className?: string;
 }
 
-function NotFound({ label }: NotFoundProps) {
-  return <div className="bg-white p-2 text-gray-600 text-lg rounded-lg">{label}</div>;
+function NotFound({ label, className = '' }: NotFoundProps) {
+  return (
+    <div
+      className={twcn('NotFound bg-white p-2 text-gray-600 text-lg rounded-lg w-full', className)}
+    >
+      {label}
+    </div>
+  );
 }
 
 export default NotFound;
