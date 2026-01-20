a7bb293aa35ae6b6d5cc8956cde3d0508366f016
Xiaokai Li <kaidev1024@gmail.com>
2026-01-19 19:42:08 -0800

    minor fix

--- src/loading/LoadingIcon.tsx
+++ src/loading/LoadingIcon.tsx
@@ -1,13 +1,15 @@
 import { twcn } from 'waujs';
 import loadingGifUrl from './loading.gif';
-import Img from '../html/Img';
+import { StaticImg } from 'src/static';
 
 interface LoadingIconProps {
   className?: string;
 }
 
 function LoadingIcon({ className = '' }: LoadingIconProps) {
-  return <Img src={loadingGifUrl} alt="loading" className={twcn('LoadingIcon w-5', className)} />;
+  return (
+    <StaticImg src={loadingGifUrl} alt="loading" className={twcn('LoadingIcon w-5', className)} />
+  );
 }
 
 export default LoadingIcon;
